c81b726de421bdc10462935059486d9bed7d5417
Adobea Essien <reginaessien83@gmail.com>
2025-08-31 09:37:34 +0000

    mlops structure

---
+++ app.py
@@ -0,0 +1,4 @@
+import pandas as pd
+from churn.logger import logging
+
+logging.info("Richard taught me this")
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="churn",
+    version="0.0.0",
+    author="Adobea",
+    author_email="reginaessien83@gmail.com",
+    packages=find_packages()
+)
